807d5c0683b78cbb5bf43f3ef7e982785069c052
Zereges <zereges@gmail.com>
2015-05-16 13:05:03 +0200

    StatTypes: cosmetics

--- src/defs/StatTypes.java
+++ src/defs/StatTypes.java
@@ -2,24 +2,24 @@ package defs;
 
 public enum StatTypes
 {
-    LEFT_MOVES("Left moves", 0),
-    RIGHT_MOVES("Right moves", 1),
-    UP_MOVES("Up moves", 2),
-    DOWN_MOVES("Down moves", 3),
-    TOTAL_MOVES("Total moves", 4),
-    BLOCKS_MOVED("Blocks moved", 5),
-    BLOCKS_MERGED("Blocks merged", 6),
-    GAME_RESTARTS("Game restarts", 7),
-    GAME_WINS("Games won", 8),
-    GAME_LOSES("Games lost", 9),
-    TOTAL_TIME_PLAYED("Total seconds spent playing", 10),
-    TOTAL_SCORE("Total score gained", 11),
-    HIGHEST_SCORE("Highest score obtained", 12),
-    MAXIMAL_BLOCK("Maximal block", 13);
+    LEFT_MOVES(0, "Left moves"),
+    RIGHT_MOVES(1, "Right moves"),
+    UP_MOVES(2, "Up moves"),
+    DOWN_MOVES(3, "Down moves"),
+    TOTAL_MOVES(4, "Total moves"),
+    BLOCKS_MOVED(5, "Blocks moved"),
+    BLOCKS_MERGED(6, "Blocks merged"),
+    GAME_RESTARTS(7, "Game restarts"),
+    GAME_WINS(8, "Games won"),
+    GAME_LOSES(9, "Games lost"),
+    TOTAL_TIME_PLAYED(10, "Total seconds spent playing"),
+    TOTAL_SCORE(11, "Total score gained"),
+    HIGHEST_SCORE(12, "Highest score obtained"),
+    MAXIMAL_BLOCK(13, "Maximal block");
     
-    private String mDesc;
-    private int mId;
-    StatTypes(String desc, int id) { mDesc = desc; mId = id; }
+    private final int mId;
+    private final String mDesc;
+    StatTypes(int id, String desc) { mId = id; mDesc = desc; }
     
     @Override
     public String toString() { return mDesc; }
